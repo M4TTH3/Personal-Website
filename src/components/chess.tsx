35e825848bef445ec7534f205b3cfe0ef345033e
M4TTH3 <matthewau-yeung@hotmail.com>
2024-09-11 11:38:10 -0400

    added proper environment

--- src/components/chess.tsx
+++ src/components/chess.tsx
@@ -5,7 +5,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { io } from "socket.io-client";
  
 const socket = io(
-    process.env.CHESS_SOCKET_URL ?? "http://localhost:3001",
+    process.env.NEXT_PUBLIC_SOCKET_URL ?? "http://localhost:3001",
     {
         transports: ["websocket"],
         upgrade: false
